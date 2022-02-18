e8623ef2eaafcce966c03aa6deeb19960feb1e17
KENZO TSUJI <58035269+tsujike@users.noreply.github.com>
2022-02-19 00:13:26 +0900

    1_2

---
+++ Chapter1/1_2.java
@@ -0,0 +1,12 @@
+public class Sample1_2 {
+
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int i = 1; i <= 5; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+
+}
